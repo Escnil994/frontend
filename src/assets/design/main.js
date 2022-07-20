a21f4553f01ab6778fe758a36821ab922336974b
Escnil994-Windows <escnil994@gmail.com>
2022-07-19 22:55:29 -0600

    Adding design in assets

---
+++ src/assets/design/main.js
@@ -0,0 +1,12 @@
+document.querySelector('.menu-btn').addEventListener('click', function (){
+    document.querySelector('.nav-menu').classList.toggle('show')
+});
+
+ScrollReveal().reveal('.showcase')
+
+ScrollReveal().reveal('.news-cards', {delay: 500})
+
+ScrollReveal().reveal('.card-banner-one', {delay: 500})
+
+ScrollReveal().reveal('.card-banner-two', {delay: 500})
+
